64210e15c98b6ce02b4477994add0f54c1827e0d
mircea814 <mirceacristian814@yahoo.com>
2020-07-03 15:12:02 +0300

    Add another 2 classes

---
+++ HolidayManagement/Models/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HolidayManagement.Models
+{
+    public class Request
+    {
+        public int RequestID { get; set; }
+        public int StaffId { get; set; }
+        public Staff Staff { get; set; }
+        public string RequestStatus { get; set; }
+        public RequestStatus Status { get; set; }
+    }
+}
